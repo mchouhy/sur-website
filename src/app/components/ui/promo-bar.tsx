f088a2a4f0b4fe3eefc4772b5ea4a7ca2b040a19
mchouhy <marianochouhy@gmail.com>
2025-07-16 17:10:45 +0200

    first commit

---
+++ src/app/components/ui/promo-bar.tsx
@@ -0,0 +1,7 @@
+export default function PromoBar() {
+  return (
+    <aside className="w-full bg-black text-white text-center py-1 text-sm">
+      ¡10% DE DESCUENTO EN TU PRIMERA COMPRA!
+    </aside>
+  );
+}
