ba3cc9839b25cfdcdac4bdcecde7a8a2a550f860
Mariano Chouhy <marianochouhy@gmail.com>
2025-08-19 16:56:25 +0200

    WIP: move header & promobar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
+import HeartSvgIcon from "@/components/icons/HeartSvgIcon";
+import ShoppingBagSvgIcon from "@/components/icons/ShoppingBagSvgIcon";
+import UserSvgIcon from "@/components/icons/UserSvgIcon";
+import MobileNavBar from "@/components/layout/mobile-navbar";
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Saira, Yrsa } from "next/font/google";
+import PromoBar from "@/components/ui/promo-bar";
 
 const saira = Saira({
   variable: "--font-saira",
@@ -20,7 +27,50 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={`font-saira antialiased`}>{children}</body>
+      <body className={`font-saira antialiased`}>
+        <PromoBar />
+        <header className="mx-auto flex max-w-7/12 items-center justify-between gap-x-8 py-4 md:justify-items-start md:py-5">
+          <MobileNavBar />
+          {/* Add SVG logo here */}
+          <h1 className="text-center text-2xl text-nowrap">Luz & Mariano</h1>
+          <form
+            action="submit"
+            className="relative flex h-11 w-full max-w-5xl items-center rounded-md bg-[#EBEBEB]"
+          >
+            <Input
+              className="h-full border-transparent px-6 text-[#747474] focus-visible:ring-[2px] focus-visible:ring-gray-300"
+              placeholder="Buscar mesas, sillas y más"
+            />
+            <SearchIcon
+              strokeWidth={1.5}
+              size={20}
+              className="absolute right-3"
+            />
+          </form>
+          <ul className="flex items-center justify-center">
+            <li className="inline-block px-2">
+              <a href="/profile" className="text-gray-700 hover:text-gray-900">
+                <UserSvgIcon width={28} />
+              </a>
+            </li>
+            {/* <li className="inline-block px-2">
+              <a
+                href="/favorites"
+                className="text-gray-700 hover:text-gray-900"
+              >
+                <HeartSvgIcon width={28} />
+              </a>
+            </li> */}
+            <li className="inline-block px-2">
+              <a href="/cart" className="text-gray-700 hover:text-gray-900">
+                <ShoppingBagSvgIcon width={28} />
+              </a>
+            </li>
+          </ul>
+        </header>
+        <hr className="border-0 border-b border-gray-200" />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,37 +4,8 @@ import { Input } from "@/components/ui/input";
 import ShoppingBagSvgIcon from "@/components/icons/ShoppingBagSvgIcon";
 import HeartSvgIcon from "@/components/icons/HeartSvgIcon";
 import UserSvgIcon from "@/components/icons/UserSvgIcon";
+import { SearchIcon } from "lucide-react";
 
 export default function Home() {
-  return (
-    <main>
-      <PromoBar />
-      <header className="flex items-center justify-between gap-x-4 border-b border-gray-200 px-36 py-4 md:py-5">
-        <MobileNavBar />
-        {/* Add SVG logo here */}
-        <h1 className="text-center text-2xl text-nowrap">Luz & Mariano</h1>
-        <Input
-          className="h-11 max-w-5xl border-transparent bg-[#EBEBEB] px-4 text-[#747474] focus-visible:ring-[2px] focus-visible:ring-gray-300"
-          placeholder="Buscar mesas, sillas y más"
-        />
-        <ul className="flex items-center justify-center">
-          <li className="inline-block px-2">
-            <a href="/profile" className="text-gray-700 hover:text-gray-900">
-              <UserSvgIcon width={28} />
-            </a>
-          </li>
-          <li className="inline-block px-2">
-            <a href="/favorites" className="text-gray-700 hover:text-gray-900">
-              <HeartSvgIcon width={28} />
-            </a>
-          </li>
-          <li className="inline-block px-2">
-            <a href="/cart" className="text-gray-700 hover:text-gray-900">
-              <ShoppingBagSvgIcon width={28} />
-            </a>
-          </li>
-        </ul>
-      </header>
-    </main>
-  );
+  return <main></main>;
 }
