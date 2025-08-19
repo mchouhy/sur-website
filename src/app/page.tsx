448856a561ac98ec81083577616c4ce0fe9b6aed
mchouhy <marianochouhy@gmail.com>
2025-08-19 14:14:47 +0200

    WIP header: adds search bar input & user, favorites and cart icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,39 @@
 import PromoBar from "../components/ui/promo-bar";
 import MobileNavBar from "../components/layout/mobile-navbar";
+import { Input } from "@/components/ui/input";
+import ShoppingBagSvgIcon from "@/components/icons/ShoppingBagSvgIcon";
+import HeartSvgIcon from "@/components/icons/HeartSvgIcon";
+import UserSvgIcon from "@/components/icons/UserSvgIcon";
 
 export default function Home() {
   return (
     <main>
       <PromoBar />
-      <header className="flex items-center py-2">
+      <header className="flex items-center justify-between gap-x-4 border-b border-gray-200 px-36 py-4 md:py-5">
         <MobileNavBar />
         {/* Add SVG logo here */}
-        <h1 className="text-center text-2xl">Luz & Mariano</h1>
+        <h1 className="text-center text-2xl text-nowrap">Luz & Mariano</h1>
+        <Input
+          className="h-11 max-w-5xl border-transparent bg-[#EBEBEB] px-4 text-[#747474] focus-visible:ring-[2px] focus-visible:ring-gray-300"
+          placeholder="Buscar mesas, sillas y más"
+        />
+        <ul className="flex items-center justify-center">
+          <li className="inline-block px-2">
+            <a href="/profile" className="text-gray-700 hover:text-gray-900">
+              <UserSvgIcon width={28} />
+            </a>
+          </li>
+          <li className="inline-block px-2">
+            <a href="/favorites" className="text-gray-700 hover:text-gray-900">
+              <HeartSvgIcon width={28} />
+            </a>
+          </li>
+          <li className="inline-block px-2">
+            <a href="/cart" className="text-gray-700 hover:text-gray-900">
+              <ShoppingBagSvgIcon width={28} />
+            </a>
+          </li>
+        </ul>
       </header>
     </main>
   );
---
+++ src/components/icons/HeartSvgIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+type Props = React.SVGProps<SVGSVGElement>;
+
+export default function HeartIcon(props: Props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+    </svg>
+  );
+}
